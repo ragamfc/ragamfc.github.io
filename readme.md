3d1562223226e8bcda04f027c81ebd6956d4965c
Edy Tanto <edytanto.chai@gmail.com>
2024-10-16 00:25:52 +0700

    add hero section

---
+++ readme.md
@@ -0,0 +1,5 @@
+images:
+Photo by <a href="https://unsplash.com/@dominikkuhn?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Dominik Kuhn</a> on <a href="https://unsplash.com/photos/soccer-players-on-green-game-field-kEHp1gUJNO8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+
+  
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+            screens: {
+                sm: '100%',
+                md: '100%',
+                lg: '100%',
+                xl: '100%',
+                '2xl': '1600px',
+            }
+        },
+        extends: {
+            colors: {
+                "ragam_red": "#d31145",
+            }
+        }
+    }
+}
