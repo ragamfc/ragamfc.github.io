9403145b711aa62146b51c4f9b4c9d6f5ad563ec
Edy Tanto <edytanto.chai@gmail.com>
2024-11-03 15:48:04 +0700

    feat: fix match score overlap on lg page

--- app/fun-football-community-league-2024/hasil-pertandingan/page.tsx
+++ app/fun-football-community-league-2024/hasil-pertandingan/page.tsx
@@ -1,10 +1,23 @@
 import { MatchScore } from "@/components/internals/match-score";
+import Image from "next/image";
 
 export default function HasilPertandingan() {
   return (
     <>
-      <nav className="flex flex-col md:flex-row md:justify-between md:h-20 bg-gradient-to-l from-red-900 to-rose-900">
-        <div className="px-4 py-2 h-full flex items-center">Ragam FC</div>
+      <nav className="flex flex-col md:flex-row md:justify-between md:h-20 bg-gradient-to-l from-red-900 to-rose-700">
+        <div className="px-4 py-2 h-full flex gap-2 items-center">
+          <Image
+            src="/images/ragam-fc-removebg-preview-shadow.png"
+            width={64}
+            height={64}
+            alt="ragam-fc-logo"
+            className="bg-white rounded-full"
+          />
+          <div>
+            <div className="text-xl leading-tight font-bold text-white">Ragam Football Club</div>
+            <div className="text-lg leading-none font-bold text-white">Fun Football Community League 2024</div>
+          </div>
+        </div>
         <ul className="flex gap-8 px-4 py-2 h-full text-white overflow-x-scroll text-nowrap items-center font-bold">
           <li>Home</li>
           <li>Klasemen</li>
@@ -23,7 +36,7 @@ export default function HasilPertandingan() {
           <div className="text-center text-white text-xl pb-4">
             Sabtu, 5 November 2024
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-3">
             <MatchScore />
             <MatchScore />
             <MatchScore />
